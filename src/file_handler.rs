532ec6f54579edc0e8cba5cd861543fffddea07b
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2023-07-20 21:15:14 +0200

    Added Regex | updated print_scr_files | Added get_file_paths | added is_part_of_pattern

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use regex::Regex;
 
 pub struct MvObj {
     pub source: String,
@@ -10,8 +11,9 @@ pub struct MvObj {
 pub trait Mover {
     fn create(source: String, target: String, pattern: String, exclude: bool) -> MvObj;
     fn create_ref_str(source: &str, target: &str, pattern: &str, exclude: bool) -> MvObj;
-    fn print_info(&self);
     fn print_src_files(&self);
+    fn get_file_paths(&self) -> Vec<String>;
+    fn is_part_of_pattern(&self, path: &String) -> bool;
 }
 
 impl Mover for MvObj {
@@ -26,26 +28,52 @@ impl Mover for MvObj {
     fn create_ref_str(source: &str, target: &str, pattern: &str, exclude: bool) -> MvObj {
         Self::create(source.to_string(), target.to_string(), pattern.to_string(), exclude)
     }
-    fn print_info(&self) {
-        println!("src: {} | dest: {} | ptrn: {} | excl: {}", self.source, self.target, self.pattern, self.exclude)
-    }
     fn print_src_files(&self) {
+        let paths: Vec<String> = self.get_file_paths();
+        for path in paths {
+            println!("Path: {}", path);
+        }
+    }
+    fn get_file_paths(&self) -> Vec<String> {
         let path = Path::new(self.source.as_str());
         let result = path.read_dir();
+        let mut file_paths: Vec<String> = vec![];
         match result {
             Ok(result) => {
-                for entry in result{
+                for entry in result {
                     match entry {
                         Ok(entry) => {
-                            println!("File: {} | Path: {} | File?: {:?} | Metadata: {:?}",entry.file_name().to_str().unwrap(),entry.path().to_str().unwrap(),entry.file_type().unwrap(),entry.metadata().unwrap())
-
+                            let raw_file_path = entry.path();
+                            let unfettered_file_path = raw_file_path.as_path().to_str();
+                            let file_path: String;
+                            if unfettered_file_path.is_some() {
+                                file_path = unfettered_file_path.unwrap().to_string();
+                            } else {
+                                file_path = "".to_string();
+                            }
+                            if self.is_part_of_pattern(&file_path) {
+                                file_paths.insert(file_paths.len(), file_path);
+                            }
                         }
-                        _ => {}
+                        _ => { println!("Exception during Folder Scan") }
                     };
                 }
-
             }
-            _ => {}
+            _ => { println!("Exception during Folder Definition"); }
         };
+        file_paths
+    }
+
+    fn is_part_of_pattern(&self, path: &String) -> bool {
+        let regex_pattern = Regex::new(self.pattern.as_str());
+        match regex_pattern {
+            Ok(regex_pattern) => {
+                regex_pattern.is_match(path.as_str())
+            }
+            _ => {
+                println!("Exception during Pattern initialisation");
+                false
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use crate::file_handler::{Mover, MvObj};
 mod file_handler;
 
 fn main() {
-    let movable_test= MvObj::create_ref_str(".","..","*",false);
-    movable_test.print_info();
+    let movable_test= MvObj::create_ref_str(".","..",".*",false);
     movable_test.print_src_files();
 }
